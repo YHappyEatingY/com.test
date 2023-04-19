f21c6ac7541a8669e9d072f3d668e98977b39695
ybandby <2458438711@qq.com>
2023-04-19 13:44:39 +0800

    test

---
+++ src/main/java/com/example/this_is_a_test/controller.java
@@ -0,0 +1,23 @@
+package com.example.this_is_a_test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
+
+/**
+ * @author Happy Day !
+ * @createTime 2023-04-19 13:17
+ */
+@RestController
+@RequestMapping("/hello")
+public class controller {
+
+    @GetMapping
+    public String hello(){
+        System.out.println("你好!");
+        return "hello";
+    }
+
+}
---
+++ src/test/java/com/example/this_is_a_test/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.this_is_a_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
